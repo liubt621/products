709621f935d7fe2fb9a97296aa9bbe9439f98b28
liubt621 <liuuul0621@gmail.com>
2023-04-30 13:51:16 +0800

    second commit

--- p.py
+++ p.py
@@ -3,9 +3,6 @@ while True:
 	name = input("請輸入商品名稱:")
 	if name == "q":
 		break
-	price = input("請輸入商品價格")
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)
+	price = input("請輸入商品價格:")
+	products.append([name,price])
 print(products)
